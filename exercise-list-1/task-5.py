db309f95ce8c0da14b0e6cce533970a24151f4e5
Henrique Rosset <henriquerosset@hotmail.com>
2021-04-09 00:27:15 -0300

    commit task-4 and task-5

---
+++ exercise-list-1/task-5.py
@@ -0,0 +1,12 @@
+while True:
+    ano = int(input('Digite um ano para descobrir se ele é bissexto:\n'))
+
+    if ano % 4 == 0:
+        if ano % 100 != 0:
+            print('\n O ano digitado é um ano bissexto')
+        elif ano % 400 == 0:
+                print('\n O ano digitado é um ano bissexto')
+        else:
+            print('\n O ano digitado não é um ano bissexto')
+    else:
+        print('\n O ano digitado não é um ano bissexto\n')
