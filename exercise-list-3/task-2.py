a220befcc05d4235ed41b4b8dd06e4ccfd4c1455
Henrique Rosset <henriquerosset@hotmail.com>
2021-05-19 21:43:51 -0300

    ELECTRE: add ranking method

--- exercise-list-3/task-2.py
+++ exercise-list-3/task-2.py
@@ -1,4 +1,4 @@
-from numpy import array, power, sqrt, zeros, absolute, empty_like, argsort
+from numpy import array, power, sqrt, zeros, absolute, empty_like, argsort, arange
 
 class ELECTRE(object):
     """docstring for ELimination and Et Chouce Translating REality (ELECTRE)"""
@@ -13,8 +13,11 @@ class ELECTRE(object):
         self.wn_matrix = empty_like(matrix)
         self.concordance_matrix = zeros([self.alternatives,self.alternatives])
         self.discordance_matrix = empty_like(self.concordance_matrix)
-        self.pure_concordance_matrix = zeros([self.alternatives,1])
-        self.pure_discordance_matrix = zeros([self.alternatives,1])
+        self.pure_concordance_index = zeros([self.alternatives,1])
+        self.pure_discordance_index = zeros([self.alternatives,1])
+        self.pure_concordance_index_rank = zeros([self.alternatives,1])
+        self.pure_discordance_index_rank = zeros([self.alternatives,1])
+        self.index_rank = zeros([self.alternatives,1])
 
     def matrix_normalize(self):
         #Evaluate the Eq.(11) denominator for each column
@@ -57,19 +60,31 @@ class ELECTRE(object):
                         tmp_max[0][j] = self.wn_matrix[b][j]-self.wn_matrix[a][j]
                 self.discordance_matrix[a][b] = tmp_max.max()/(absolute(tmp_abs).max())
 
-    def pure_concordance_matrix_eval(self):
-        self.pure_concordance_matrix = self.concordance_matrix.sum(axis=1)-self.concordance_matrix.sum(axis=0)
+    def pure_concordance_index_eval(self):
+        self.pure_concordance_index = self.concordance_matrix.sum(axis=1)-self.concordance_matrix.sum(axis=0)
 
-    def pure_discordance_matrix_eval(self):
-        self.pure_discordance_matrix = self.discordance_matrix.sum(axis=1)-self.discordance_matrix.sum(axis=0)          
+    def pure_discordance_index_eval(self):
+        self.pure_discordance_index = self.discordance_matrix.sum(axis=1)-self.discordance_matrix.sum(axis=0)          
+
+    def rank(self):
+        tmp = -self.pure_concordance_index.argsort()
+        self.pure_concordance_index_rank = empty_like(tmp)
+        self.pure_concordance_index_rank[tmp] = arange(len(self.pure_concordance_index_rank))
+        tmp = self.pure_discordance_index.argsort()
+        self.pure_discordance_index_rank = empty_like(tmp)
+        self.pure_discordance_index_rank[tmp] = arange(len(self.pure_discordance_index_rank))
+        self.index_rank = (self.pure_concordance_index_rank + self.pure_concordance_index_rank)/2
+        tmp = self.index_rank
+        self.index_rank[tmp] = arange(len(self.pure_discordance_index_rank))
 
     def solve(self):
         self.matrix_normalize()
         self.weighted_eval()
         self.concordance_matrix_eval()
         self.discordance_matrix_eval()
-        self.pure_concordance_matrix_eval()
-        self.pure_discordance_matrix_eval()
+        self.pure_concordance_index_eval()
+        self.pure_discordance_index_eval()
+        self.rank()
 
 matrix = array([[60, 0.40, 2540, 500, 990],
     [6.35,0.15,1016,3000,1041],
@@ -89,3 +104,5 @@ weights = array([0.036,0.192,0.326,0.326,0.120])
 decision = ELECTRE(matrix,weights,non_benefitial_att=[1])
 
 decision.solve()
+
+print(decision.index_rank)
