324ff2d7d68e6ad0285ab051db7ba0b8433c81b6
Henrique Rosset <henriquerosset@hotmail.com>
2021-07-08 20:35:05 -0300

    Minor code cleaning

--- exercise-list-6/TravelingSalesman.py
+++ exercise-list-6/TravelingSalesman.py
@@ -9,8 +9,9 @@ class NearestNeighbour(object):
     
     def __init__(self,initial_node,cost_matrix):
         self.initial_node = initial_node
-        self.cost_matrix = np.array(cost_matrix)
-        self.n = self.cost_matrix.shape[0]
+        self.cost_matrix = cost_matrix
+        self.cost_matrix_array = np.array(cost_matrix)
+        self.n = self.cost_matrix_array.shape[0]
         self.path = []
         self.path_costs = []
         self.total_cost = 0
@@ -24,7 +25,7 @@ class NearestNeighbour(object):
         return np.where(array == value)[0][0]     
     
     def get_nearest_neighbour(self, node):
-        neighbours_distance = self.cost_matrix[node-1]
+        neighbours_distance = self.cost_matrix_array[node-1]
         nearest_neighbour = self.get_position_of(
             value = self.minimum_non_zero_value(neighbours_distance),
             array = neighbours_distance)
@@ -35,14 +36,14 @@ class NearestNeighbour(object):
         while len(self.path) < self.n:
             actual_node = self.path[len(self.path)-1]
             nearest_neighbour = self.get_nearest_neighbour(actual_node)
-            self.cost_matrix[:,actual_node-1].fill(0)
+            self.cost_matrix_array[:,actual_node-1].fill(0)
             self.path.append(nearest_neighbour+1)
         self.path.append(self.initial_node)
 
     def get_total_distance(self):       
         for i in range(0,len(self.path)-1):
             self.path_costs.append(
-                self.cost_matrix[self.path[i]-1,self.path[i+1]-1]
+                self.cost_matrix[self.path[i]-1][self.path[i+1]-1]
             )
         self.total_cost = np.array(self.path_costs).sum()
         pass     
@@ -57,11 +58,14 @@ if __name__ == '__main__':
                    [2,0,3,5,3],
                    [1,3,0,3,2],
                    [7,5,3,0,4],
-                   [4,3,2,4,0]] 
+                   [4,3,2,4,0]]
+
     initial_node = 1
+
     example = NearestNeighbour(initial_node=1,
                                cost_matrix = cost_matrix)
-    example.solve()
-    print(f'The path is: {example.path}\n',
-        f'With a total cost of {example.total_cost}')  
 
+    example.solve()
+
+    print(f'The path is: {example.path}\n',
+        f'With a total cost of {example.total_cost}')
