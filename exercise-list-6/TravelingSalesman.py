2be49869ccae88d92e95b6444b9f7b425011b40a
Henrique Rosset <henriquerosset@hotmail.com>
2021-07-09 11:46:14 -0300

    minor code cleaning

--- exercise-list-6/TravelingSalesman.py
+++ exercise-list-6/TravelingSalesman.py
@@ -1,5 +1,4 @@
 import numpy as np
-from numpy.core.records import array
 
 class NearestNeighbour(object):
     """docstring for for the Travelling Salesman Problem (TSP) 
@@ -54,9 +53,9 @@ class NearestNeighbour(object):
     
 if __name__ == '__main__':
 
-    cost_matrix = [[0,2,1,7,4],
+    cost_matrix = [[0,2,0,7,4],
                    [2,0,3,5,3],
-                   [1,3,0,3,2],
+                   [0,3,0,3,2],
                    [7,5,3,0,4],
                    [4,3,2,4,0]]
 
